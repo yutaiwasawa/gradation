ca3be877dcd2a64df23901ab9903fed03ac5bda5
yutaiwasawa <yutaiwasawa1212@gmail.com>
2020-11-14 01:03:46 +0900

    ディレクトリ整理

---
+++ src/dataset.js
@@ -0,0 +1,43 @@
+const defaultDataset = {
+  "gradation": {
+    "linear": [
+      {
+        "deg": 43,
+        "colors": [
+          { "color": "#4158D0", "position": 0 },
+          { "color": "#C850C0 ", "position": 46 },
+          { "color": "#FFCC70", "position": 100 }
+        ],
+        "fallback": "#4158D0"
+      },
+      {
+        "deg": 160,
+        "colors": [
+          { "color": "#0093E9", "position": 0 },
+          { "color": "#80D0C7 ", "position": 100 }
+        ],
+        "fallback": "#0093E9"
+      },
+      {
+        "deg": 62,
+        "colors": [
+          { "color": "#8EC5FC", "position": 0 },
+          { "color": "#E0C3FC ", "position": 100 }
+        ],
+        "fallback": "#0093E9"
+      }
+    ],
+    "radial": [
+      {
+        "colors": [
+          { "color": "#4158D0", "position": 0 },
+          { "color": "#C850C0 ", "position": 46 },
+          { "color": "#FFCC70", "position": 100 }
+        ],
+        "fallback": "#4158D0"
+      }
+    ]
+  }
+}
+
+export default defaultDataset;
